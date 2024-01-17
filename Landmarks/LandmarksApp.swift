048b34ba1426b9974382bb9592e40167319a8eda
Wang Zhigao <aimiskamei@gmail.com>
2024-01-17 18:39:16 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Wang Zhigao on 2024/1/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
